abbfa379e52a3b863b6c82e931bf0c4f6ecd6f89
VanhelsingX3 <galexbh@yahoo.com.mx>
2020-04-03 00:18:08 -0600

    Conectando mongo a nuestra restful API

--- Project/src/app.ts
+++ Project/src/app.ts
@@ -2,6 +2,11 @@ import express,{Application} from "express";
 import {MainController} from "./controller/main.controller"
 import bodyParse from "body-parser";
 import cors from "cors";
+import mongoose from "mongoose"
+
+import {config} from "dotenv";
+import {resolve} from "path";
+config({path:resolve(__dirname,"../.env")});
 
 class App{
     public app: Application;
@@ -9,12 +14,23 @@ class App{
     constructor(){
         this.app = express();
         this.setConfig();
+        this.setMongoDDConfig();
         this.mainController = new MainController(this.app);
     }
     private setConfig(){
         this.app.use(bodyParse.json({limit:"50mb"})); //Los objetos que vamos a estar enviando
         this.app.use(bodyParse.urlencoded({limit:"50mb", extended: true})); // body x-www-form-urlencoded | formularios
         this.app.use(cors());
+    }
+    private setMongoDDConfig(){
+        mongoose.Promise = global.Promise;
+        mongoose.connect(process.env.MONGO_URI!,{ useNewUrlParser:true, useUnifiedTopology: true },(err: any)=>{
+            if(err){
+                console.log(err.message);
+            }else{
+                console.log("Conexion exitosa");
+            }  
+        })
     }
 }
 
--- Project/src/server.ts
+++ Project/src/server.ts
@@ -1,4 +1,8 @@
 import app from "./app";
+import {config} from "dotenv";
+import {resolve} from "path";
+
+config({path:resolve(__dirname,"../../.env")});
 
 app.listen(3000, ()=>{
     console.log("http://localhost:3000/");
